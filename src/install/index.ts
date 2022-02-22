39daeb14d80368170f46b5d51efeb9143132f08b
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-02-22 16:55:41 +0800

    fix: disable fetching already-fetched url

--- src/install/index.ts
+++ src/install/index.ts
@@ -1,20 +1,28 @@
 import * as fetch from 'node-fetch';
 import 'colors';
 import { outputFileSync } from 'fs-extra';
-import { join } from 'path';
+import path, { join } from 'path';
 import { InstallOptions, Plugin } from '../types/options';
 import Compile, { getModules, init } from '../compiler/compile';
-import RandomString from '../../lib/randomString';
+
+let history = [];
 
 export default async function InstallModule(url: string, options?: InstallOptions): Promise<void> {
+  if (history.includes(url)) return;
+  options = { extension: 'js', plugins: [], ...options };
+
   const start = performance.now();
   init();
   options.plugins.forEach(async (plugin: Plugin) => {
     if (plugin.transformImportUrl) url = await plugin.transformImportUrl(url);
   });
+  if (options.debug) console.log(`${'info'.blue} fetching ${url}`);
+
+  const parsed = path.parse(url);
 
   const response = await fetch.default(url);
   if (response.status === 200) {
+    history.push(url);
     const code = await response.text();
     outputFileSync(join(process.cwd(), `./.whiski/${new URL(url).pathname}`), code);
     console.log(
@@ -23,7 +31,14 @@ export default async function InstallModule(url: string, options?: InstallOption
       }`
     );
     Compile(code, url, options);
-  } else {
-    console.log(`${'fail'.red.bold} install ${url.bold}`);
+  } else if (!history.includes(`${parsed.dir}/${parsed.name}/index.${options.extension}`)) {
+    await InstallModule(`${parsed.dir}/${parsed.name}/index.${options.extension}`, options);
+    history.push(`${parsed.dir}/${parsed.name}/index.${options.extension}`);
+  } else if (!history.includes(url)) {
+    console.log(`${'failed'.red.bold} install ${url.bold}`);
   }
+}
+
+export function refresh() {
+  history = [];
 }
