6432c18f07d744a1979148670e60647ff49ae902
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-02-24 22:36:21 +0800

    delete build scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,21 +1,6 @@
 const { build } = require('esbuild');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
-const fs = require('fs');
-const { join } = require('path');
-
-const prod = fs.readFileSync(join(__dirname, '../build-package.json'), 'utf8');
-const typing = fs.readFileSync(join(__dirname, '../whiskijs.d.ts'), 'utf8');
-const readme = fs.readFileSync(join(__dirname, '../README.md'), 'utf8');
-
-// For Publish
-
-fs.writeFileSync(join(__dirname, '../dist/whiskijs.d.ts'), typing);
-fs.writeFileSync(join(__dirname, '../dist/package.json'), prod);
-fs.writeFileSync(join(__dirname, '../dist/README.md'), readme);
-
-/* client */
-
 build({
   entryPoints: ['src/index.ts'],
   bundle: true,
