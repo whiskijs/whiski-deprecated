480d22bfa5820c03cb7920c9f9107e4732b252c2
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-02-24 22:03:40 +0800

    rm ts plugin

--- plugins/worker.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-return-await */
-export default async function pluginworker(func, ...args: any[]): Promise<any> {
-  if (typeof func !== 'function') {
-    throw new Error('pluginworker: first argument must be a function');
-  }
-  return await func(...args);
-}
--- test/compile.test.ts
+++
@@ -1,7 +0,0 @@
-import TypescriptPlugin from '../plugins/typescript';
-import Compile from '../src/compiler/compile';
-
-test('Typescript Plugin', async () => {
-  const compiled = await (await Compile('const a: string = "hello";', 'test.ts', { plugins: [TypescriptPlugin()] })).code;
-  expect(compiled.split(/\r?\n/).join('')).toEqual('"use strict";const a = "hello";');
-});
