1ecff6fa121bebaeb860afc660730fb87500223a
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-02-24 19:34:51 +0800

    Update compile.test.ts

--- test/compile.test.ts
+++ test/compile.test.ts
@@ -2,6 +2,6 @@ import TypescriptPlugin from '../plugins/typescript';
 import Compile from '../src/compiler/compile';
 
 test('Typescript Plugin', async () => {
-  const compiled = await Compile('const a: string = "hello";', 'test.ts', { plugins: [TypescriptPlugin()] });
+  const compiled = await (await Compile('const a: string = "hello";', 'test.ts', { plugins: [TypescriptPlugin()] })).code;
   expect(compiled.split(/\r?\n/).join('')).toEqual('"use strict";const a = "hello";');
 });
