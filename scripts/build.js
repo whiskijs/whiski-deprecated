85fd3c0b70c54a3c20be00eb3a571ea6456ee06e
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-02-24 22:03:23 +0800

    build: edit build script for production

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,19 @@
 const { build } = require('esbuild');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
+const fs = require('fs');
+const { join } = require('path');
+
+const prod = fs.readFileSync(join(__dirname, '../build-package.json'), 'utf8');
+const typing = fs.readFileSync(join(__dirname, '../whiskijs.d.ts'), 'utf8');
+const readme = fs.readFileSync(join(__dirname, '../README.md'), 'utf8');
+
+// For Publish
+
+fs.writeFileSync(join(__dirname, '../dist/whiskijs.d.ts'), typing);
+fs.writeFileSync(join(__dirname, '../dist/package.json'), prod);
+fs.writeFileSync(join(__dirname, '../dist/README.md'), readme);
+
 /* client */
 
 build({
@@ -10,6 +23,8 @@ build({
   outfile: 'dist/index.js',
   plugins: [nodeExternalsPlugin()],
 });
+
+/*
 build({
   entryPoints: ['plugins/typescript/index.ts'],
   bundle: true,
@@ -17,3 +32,4 @@ build({
   outfile: 'dist/plugins/typescript.js',
   plugins: [nodeExternalsPlugin()],
 });
+*/
